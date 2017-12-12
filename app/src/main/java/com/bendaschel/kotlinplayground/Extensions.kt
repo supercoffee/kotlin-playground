551766c5890159777351cafacf8cc5f8d8d1086f
Benjamin Daschel <coffeemaxed@gmail.com>
2017-12-12 00:42:19 -0800

    refactor

--- app/src/main/java/com/bendaschel/kotlinplayground/Extensions.kt
+++ app/src/main/java/com/bendaschel/kotlinplayground/Extensions.kt
@@ -1,6 +1,8 @@
 package com.bendaschel.kotlinplayground
 
 import android.app.Activity
+import android.graphics.Color
+import android.support.v7.graphics.Palette
 
 val Activity.redditApi: RedditApi
     get() = retrofit2.Retrofit.Builder()
@@ -8,3 +10,21 @@ val Activity.redditApi: RedditApi
             .addConverterFactory(retrofit2.converter.gson.GsonConverterFactory.create())
             .build()
             .create(com.bendaschel.kotlinplayground.RedditApi::class.java)
+
+val Palette.preferredColor: Int
+    get() {
+        val methodOrder: List<(Int) -> Int> = listOf(
+                this::getDarkVibrantColor,
+                this::getDarkMutedColor,
+                this::getLightVibrantColor,
+                this::getLightMutedColor,
+                this::getDominantColor
+        )
+        var bestColor: Int = Color.WHITE
+
+        methodOrder.forEach {
+            bestColor = it(bestColor)
+        }
+
+        return bestColor
+    }
--- app/src/main/java/com/bendaschel/kotlinplayground/ThreadActivity.kt
+++ app/src/main/java/com/bendaschel/kotlinplayground/ThreadActivity.kt
@@ -2,31 +2,26 @@ package com.bendaschel.kotlinplayground
 
 import android.app.Activity
 import android.graphics.Bitmap
+import android.graphics.Color
 import android.os.Bundle
+import android.support.v7.graphics.Palette
+import android.support.v7.widget.CardView
 import android.util.Log
 import com.squareup.picasso.Picasso
 import com.squareup.picasso.Transformation
+import kotlinx.android.synthetic.main.activity_thread.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import kotlinx.android.synthetic.main.activity_thread.*
-import org.jetbrains.anko.doAsyncResult
-import android.R.attr.bitmap
-import android.graphics.Color
-import android.support.v7.graphics.Palette
-
 
 
 class ThreadActivity: Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_thread)
-
         val id = intent.getStringExtra("threadId")
 
-        // TODO: load thread from reddit api and display it
         redditApi.comments(id).enqueue(object : Callback<List<ListingWrapper>> {
             override fun onFailure(call: Call<List<ListingWrapper>>?, t: Throwable?) {
                 Log.e("ThreadActivity", "Failed to load thread", t)
@@ -40,22 +35,7 @@ class ThreadActivity: Activity() {
 
                 Picasso.with(this@ThreadActivity)
                         .load(data.contentUrl)
-                        // TODO: do image color blur on background
-                        .transform(object: Transformation {
-                            override fun key(): String {
-                                return data.contentUrl
-                            }
-
-                            override fun transform(source: Bitmap): Bitmap {
-                                val p = Palette.from(source).generate()
-                                val defaultColor = resources.getColor(R.color.default_image_background)
-                                thread_card.post {
-                                    thread_card.setCardBackgroundColor(p.preferredColor)
-                                }
-                                return source
-                            }
-
-                        })
+                        .transform(BackgroundColorTransformation(thread_card))
                         .into(img_post)
             }
 
@@ -63,20 +43,17 @@ class ThreadActivity: Activity() {
     }
 }
 
-val Palette.preferredColor: Int
-get() {
-    val methodOrder: List<(Int) -> Int> = listOf(
-            this::getDarkVibrantColor,
-            this::getDarkMutedColor,
-            this::getLightVibrantColor,
-            this::getLightMutedColor,
-            this::getDominantColor
-    )
-    var bestColor: Int = Color.WHITE
-
-    methodOrder.forEach {
-        bestColor = it(bestColor)
+class BackgroundColorTransformation(val card: CardView): Transformation {
+    override fun key(): String {
+        return toString()
     }
 
-    return bestColor
+    override fun transform(source: Bitmap): Bitmap {
+        val p = Palette.from(source).generate()
+        val defaultColor = card.context.resources.getColor(R.color.default_image_background)
+        card.post {
+            card.setCardBackgroundColor(p.preferredColor)
+        }
+        return source
+    }
 }
